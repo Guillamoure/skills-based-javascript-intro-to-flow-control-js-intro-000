c5bfe292fc6723434c729f6aa9c312c4d5829952
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-12 22:32:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -35,7 +35,7 @@ function switchAge(age) {
       console.log("You are a teenager")
       break;
     case 15:
-      return "You are a teenager"
+      console.log("/'You are a teenager/'")
       break;
     case 16:
       console.log("You are a teenager")
